8e16c9b8fe05174ae4c2347b154905176deb8b58
Sebastian Ramirez <sebastian.ramirezing@gmail.com>
2025-06-20 01:44:41 -0500

    fix: update database connection details in config

--- api/config.php
+++ api/config.php
@@ -1,8 +1,8 @@
 <?php
 // api/config.php
 $db = new PDO(
-    'mysql:host=localhost;dbname=admissions;charset=utf8mb4',
-    'adm_user',
+    'mysql:host=localhost;dbname=proyectoexplora_admissions;charset=utf8mb4',
+    'proyectoexplora_adm_user',
     'mwa~mcUX8DMN]@G-',
     [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
 );
